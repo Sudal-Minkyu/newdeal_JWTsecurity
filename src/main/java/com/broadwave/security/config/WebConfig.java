393e9ea1d5a8714fbc9d562f27350c9e7bcdaab9
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-07-07 14:03:28 +0900

    커밋

--- src/main/java/com/broadwave/security/config/WebConfig.java
+++ src/main/java/com/broadwave/security/config/WebConfig.java
@@ -10,13 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     // 호스트링크주입
     @Value("${newdeal.api.front_url}")
-    private String link;
+    private String front_url;
 
     //CORS에러 No 'Access-Control-Allow-Origin' header is present on the requested resource. 해결
     @Override
     public void addCorsMappings(CorsRegistry registry) {
              registry.addMapping("/auth/**") // auth 경로의대한 모든권한 허용
-                     .allowedOrigins("http://localhost:8010",link); // 허용 url
+                     .allowedOrigins("http://"+front_url,"http://192.168.0.24:8010"); // 허용 url 지겸님 ip http://192.168.0.24:8010
     }
 
 }
